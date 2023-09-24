ce4af48e4fc8eab7a17d1deb1cdb240bb5c5c461
Samruddhi Shende <105356384+SAM03x@users.noreply.github.com>
2023-09-24 20:09:41 +0530

    Update students.sql

--- Project/students.sql
+++ Project/students.sql
@@ -1,27 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2021 at 01:03 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.29
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `students`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `attendence`
@@ -238,7 +217,3 @@ ALTER TABLE `trig`
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
